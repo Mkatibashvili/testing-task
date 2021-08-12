bd6c8a0e49eb239e40943b35dc4b7d99d80b7938
Mkatibashvili <62959761+Mkatibashvili@users.noreply.github.com>
2021-08-12 17:07:24 +0400

    update

--- Task3/main.tf
+++ Task3/main.tf
@@ -1,15 +1,61 @@
-provider "aws" {
-    region = "eu-central-1"
-    access-key = "*******************"
-    secret_key = "************************"
+data "null_data_source" "values" {
+  inputs = {
+    ecs = "${data.aws_ami.ecs.id}"
+    centos7 = "${data.aws_ami.centos7.id}"
+    ubuntu2004 = "${data.aws_ami.ubuntu2004.id}"
+  }
 }
 
-variable cidr_blocks {
-    description = "cidr blocks and name tags for vpc and subnets"
-    type = list(object({
-        cidr_block = string
-        name = string
-    }))
+data "aws_ami" "ecs" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["*amazon-ecs-optimized"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["110980521913"] # Amazon
 }
 
+data "aws_ami" "centos7" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM EBS*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["110980521913"] # CentOS
+}
+
+data "aws_ami" "ubuntu1604" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-Focal-Fossa-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["110980521913"] # Ubuntu-Terraform
+}
 
