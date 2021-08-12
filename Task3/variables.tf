743abfeec8b6cea0deb7be66760c65285c8f2c75
Mkatibashvili <62959761+Mkatibashvili@users.noreply.github.com>
2021-08-12 17:08:20 +0400

    update

--- Task3/variables.tf
+++ Task3/variables.tf
@@ -1,27 +1,3 @@
-    default = "Terra-Ubuntu.pem"
-    default = "Terra-centos.pem"
-}
-
-variable instance_type_ubuntu {
-    type   = string
-    default = t2.micro
-}
-variable ami_ubuntu {
-    type  = string
-    default = "ami-05f7491af5eef733a"
-}
-
-variable instance_type_centos {
-    type   = string
-    default = t2.micro
-}
-variable ami_ubuntu {
-    type  = string
-    default = "ami-0e8286b71b81c3cc1"
-}
-
-variable vpc_cidr_block {
-
-    type = string
-    defatul = 10.0.0.0/16
+variable "distribution" { 
+  description = "Linux distribution for AWS AMI, supported: ecs, centos7, ubuntu2004"
 }
